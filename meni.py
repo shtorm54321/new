80fd4bb80534da61dbc2df4ee8f42061b56ac28d
shtorm54321 <htorm2009@gmail.com>
2025-10-23 10:22:29 +0600

    update

--- meni.py
+++ meni.py
@@ -1,3 +1,11 @@
 print("hello world")
 print("hello world")
 print("hello world")
+
+def five_numbers(num1, num2, num3, num4, num5):
+    total = num1 + num2 + num3 + num4 + num5
+    print(total)
+
+five_numbers(3,53,4,2,3)
+five_numbers(34,4,3,43,4)
+five_numbers(34,3,4,2,3)
